84e1da847b15adadd30e5c1054a77c5fdab26ced
tatiana <dp210795dtj1@gmail.com>
2020-11-06 11:43:22 +0200

    Redirect to admin(if user exist)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import {HeaderStyle, NameTitle} from './Header.style';
 import {Link} from 'react-router-dom';
 
-export default () => (
-    <HeaderStyle>
-        <NameTitle>
-            <Link to="./">News Zone</Link>
-        </NameTitle>
-        <Link to="/login">Login</Link>
-    </HeaderStyle>
-)
+export default () => {
+    const redirectTo = () => {
+        const user = localStorage.getItem('user');
+        if (user) {
+            return '/admin'
+        }
+        return '/login'
+    };
+
+    return (
+        <HeaderStyle>
+            <NameTitle>
+                <Link to="./">News Zone</Link>
+            </NameTitle>
+            <Link to={redirectTo()}>Login</Link>
+        </HeaderStyle>
+    )
+}
 
